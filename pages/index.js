7acc07cf24b8f519325beb4c6c5635f83d89efcc
Matt Steele <msteele@remedyhealthmedia.com>
2024-01-09 11:14:03 -0500

    fix: small word updates

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Index = () => (
   <div>
     <h1>document.write test</h1>
-    <p>This page is intended to test document.write in NextJS.</p>
+    <p>This page is intended to test document.write in NextJS. It appears to only work on direct lands vs using the internal router.</p>
     <h2>Instructions</h2>
     <ul>
       <li>Click <Link href="/test">this link</Link> and you will NOT see a <span style={{ backgroundColor: 'green', color: 'white'}}>green box</span> on the page.</li>
--- pages/test.js
+++ pages/test.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Page = () => (
   <div>
     <Link href='/'>back</Link>
-    <p>See the green box? No? Now <strong>refresh!</strong></p>
+    <p>If you don't see a green box below you must have arrived from the link on the homepage. Please <strong>refresh</strong> to see the green box.</p>
     <div dangerouslySetInnerHTML={{ __html: `<script type='text/javascript' src='/document.write.js' />` }} />
   </div>
 )
